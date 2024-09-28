590db9f1ee2a66012b6e64d9fa0c607d16207d39
Christiaan van Dam <hardc@localhost.localdomain>
2024-09-28 12:56:49 +0200

    Added straight for package management and fixing Linux bugs. Also native compilation support.

--- init.el
+++ init.el
@@ -1,4 +1,22 @@
-;; -*- lexical-binding: t; -*-
+(setq package-enable-at-startup nil)
+(setq straight-use-package-by-default t)
+(setq straight-vc-git-default-clone-depth 1)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
 
 ;; Basic UI Configuration --------------------------------------------------------------
 
@@ -6,7 +24,7 @@
 (defvar h4rdc0m/default-font-size 200)
 (defvar h4rdc0m/fixed-font-size 260)
 (defvar h4rdc0m/variable-font-size 290)
-(defvar h4rdc0m/fixed-font "FiraMono Nerd Font")
+(defvar h4rdc0m/fixed-font "FiraCode Nerd Font")
 (defvar h4rdc0m/variable-font "Cantarell")
 
 (setq inhibit-startup-message t)
@@ -29,25 +47,9 @@
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; Initialize package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents (package-refresh-contents))
-
-;; Initialize use-package on non-linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
 (column-number-mode)
 (global-display-line-numbers-mode t)
+(setq display-line-numbers 'relative)
 
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
@@ -56,40 +58,47 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(use-package command-log-mode)
 
+;; Configure Ivy
 (use-package ivy
+  :demand t
   :diminish
-  :demand
-  :bind (("C-s" . swiper)
-        :map ivy-minibuffer-map
-        ("TAB" . ivy-alt-done)
-        ("C-l" . ivy-alt-done)
-        ("C-j" . ivy-next-line)
-        ("C-k" . ivy-previous-line)
-        :map ivy-switch-buffer-map
-        ("C-k" . ivy-previous-line)
-        ("C-l" . ivy-done)
-        ("C-d" . ivy-switch-buffer-kill)
-        :map ivy-reverse-i-search-map
-        ("C-k" . ivy-previous-line)
-        ("C-d" . ivy-reverse-i-search-kill))
-  :config
+  :bind(("C-s" . swiper)
+	:map ivy-minibuffer-map
+	("TAB" . ivy-alt-done)
+	("C-l" . ivy-alt-done)
+	("C-l" . ivy-next-line)
+	("C-k" . ivy-previous-line)
+	:map ivy-switch-buffer-map
+	("C-l" . ivy-done)
+	("C-k" . ivy-previous-line)
+	("C-d" . ivy-switch-buffer-kill)
+	:map ivy-reverse-i-search-map
+	("C-k" . ivy-previous-line)
+	("C-d" . ivy-reverse-i-search-kill))
+  :commands ivy-mode
+  :init
   (ivy-mode 1))
 
+;; Configure Counsel after Ivy
 (use-package counsel
+  :after ivy
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-ibuffer)
          ("C-x C-f" . counsel-find-file)
          :map minibuffer-local-map
          ("C-f" . 'counsel-minibuffer-history))
+  :init (counsel-mode 1)
   :config
   (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
 
+;; Configure ivy-rich after ivy
 (use-package ivy-rich
+  :after ivy
   :init
   (ivy-rich-mode 1))
 
+;; Configure all-the-icons icon packs
 (use-package all-the-icons
   :if (display-graphic-p)
   :commands all-the-icons-install-fonts
@@ -101,13 +110,9 @@
   :if (display-graphic-p)
   :hook (dired-mode . all-the-icons-dired-mode))
 
-(use-package doom-modeline
-  :demand
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 25)))
-  
+;; Configure Doom Emacs themes
 (use-package doom-themes
-  :ensure t
+  :demand t
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
@@ -124,9 +129,17 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
+;; Configure Doom modeline
+(use-package doom-modeline
+  :demand t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 60)))
+
+;; Configure rainbow delimiters
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
+;; Configure which-key
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
@@ -134,7 +147,7 @@
   (setq which-key-idle-delay 0.3))
 
 (use-package helpful
-  :ensure t
+  :demand t
   :custom
   (counsel-describe-function-function #'helpful-callable)
   (counsel-describe-variable-function #'helpful-variable)
@@ -156,7 +169,7 @@
     "tt" '(counsel-load-theme :which-key "choose theme")))
 
 (use-package evil
-  :demand
+  :demand t
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
@@ -175,19 +188,22 @@
 
 (use-package evil-collection
   :after evil magit forge
+  :init
+  (setq forge-add-default-bindings nil)
   :config
   (evil-collection-init))
 
-(use-package hydra)
 
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
+(use-package hydra
+  :config
+  (defhydra hydra-text-scale (:timeout 4)
+    "scale text"
+    ("j" text-scale-increase "in")
+    ("k" text-scale-decrease "out")
+    ("f" nil "finished" :exit t))
 
-(h4rdc0m/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
+  (h4rdc0m/leader-keys
+   "ts" '(hydra-text-scale/body :which-key "scale text")))
 
 (use-package projectile
   :diminish projectile-mode
@@ -203,147 +219,11 @@
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
 
-
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 (use-package forge
+  :after magit
   :config
   (setq auth-sources '("~/.authinfo")))
-
-(defun h4rdc0m/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
-;; Set faces for heading levels
-(defun h4rdc0m/org-font-setup ();; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                        '(("^ *\\([-]\\) "
-                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-
-  (dolist (face '((org-level-1 . 1.2)
-		  (org-level-2 . 1.1)
-		  (org-level-3 . 1.05)
-		  (org-level-4 . 1.0)
-		  (org-level-5 . 1.1)
-		  (org-level-6 . 1.1)
-		  (org-level-7 . 1.1)
-		  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font h4rdc0m/variable-font :weight 'regular :height (cdr face)))
-  
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
-
-(use-package org
-  :hook (org-mode . h4rdc0m/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾")
-
-  (setq org-agenda-start-with-log-mode t)
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-
-  (setq org-agenda-files
-	'("~/Projects/Code/runemacs/OrgFiles/Tasks.org"
-	  "~/Projects/Code/runemacs/OrgFiles/Birthdays.org"))
-
-  (setq org-todo-keywords
-	'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-	  (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
-
-  (setq org-refile-targets
-	'(("Archive.org" :maxlevel . 1)
-	  ("Tasks.org" :maxlevel . 1)))
-
-  ;; Save org buffers after refiling
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
-  
-  (setq org-tag-alist
-       '((:startgroup)
-          ; Put mutually exclusive tags here
-	 (:endgroup)
-	 ("@errand" . ?E)
-	 ("@home" . ?H)
-	 ("@work" . ?W)
-	 ("agenda" . ?a)
-	 ("planning" . ?p)
-	 ("publish" . ?P)
-	 ("batch" . ?b)
-	 ("note" . ?n)
-	 ("idea" . ?i)))
-
-  (setq org-agenda-custom-commands
-	'(("d" "Dashboard"
-	   ((agenda "" ((org-deadline-warning-days 7)))
-	    (todo "NEXT"
-		  ((org-agenda-overriding-header "Next Tasks")))
-	    (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Tasks")))))
-
-	  ("n" "Next Tasks"
-	   ((todo "NEXT"
-		  ((org-agenda-override-header "Next Tasks")))))
-
-	  ("w" "Work Tasks" tags-todo "+work-email")
-
-	  ;; Low effort next actions
-	  ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-	   ((org-agenda-overriding-header "Low Effort Tasks")
-	    (org-agenda-max-todos 20)
-	    (org-agenda-files org-agenda-files)))
-
-	  ("w" "Workflow Status"
-	   ((todo "WAIT"
-		  ((org-agenda-overriding-header "Waiting on External")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "REVIEW"
-		  ((org-agenda-overriding-header "In Review")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "PLAN"
-		  ((org-agenda-overriding-header "In Planning")
-		   (org-agenda-todo-list-sublevels nil)
-		   (org-agenda-files org-agenda-files)))
-	    (todo "BACKLOG"
-		  ((org-agenda-overriding-header "Project Backlog")
-		   (org-agenda-todo-list-sublevels nil)
-		   (org-agenda-files org-agenda-files)))
-	    (todo "READY"
-		  ((org-agenda-overriding-header "Ready for Work")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "ACTIVE"
-		  ((org-agenda-overriding-header "Active tasks")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "COMPLETED"
-		  ((org-agenda-overriding-header "Completed tasks")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "CANC"
-		  ((org-agenda-overriding-header "Cancelled Tasks")
-		   (org-agenda-files org-agenda-files)))))))
-  
-
-  (setq org-capture-templates
-	'(("t" "Tasks / Projects"
-	   ("tt" "Task" entry 
-  (h4rdc0m/org-font-setup))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-(defun h4rdc0m/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-	visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . h4rdc0m/org-mode-visual-fill))
